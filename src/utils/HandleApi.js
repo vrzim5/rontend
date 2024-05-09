462c3b66b72b6d0d4ed974bab5b1d3208c210976
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-09 01:28:54 -0300

    fix: :bug: Troca a URL do backend de local para hospedada no Render

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://todo-app-express-backend-rtbt.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(`${baseURL}`).then(({ data }) => {
