333b820226160918a44d9598e957924e062c30ab
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-08 23:28:11 -0300

    feat: :tada: Cria projeto inicial para o desenvolvimento frontend
    Este commig cria o projeto inicial para o desenvolvimento frontend utilizando o React.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>TO-DO App</h1>
+
+        <div className="top">
+          <input type="text" placeholder="Adicione uma tarefa..." />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
